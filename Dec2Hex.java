cf2a24918cd39e4f802cf01c0357311edcc11dd5
brianrown <rbrown232@caledonian.ac.uk>
2019-10-25 11:47:22 +0100

    Improving tests for error handling

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,23 +1,31 @@
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 public class Dec2Hex {
     private static int Arg1;
+    private static boolean exited;
 
-    public static void main(String[] args) {
+    private void exit() {
+        exited = true;
+    }
+
+    public void main(String[] args) {
         if (args.length != 1) {
             System.err.println("The incorrect number of arguments have been entered. This program needs 1 argument to run");
+            exit();
         }
         else {
             try {
                 Arg1 = Integer.parseInt(args[0]);
             } catch (NumberFormatException e) {
-                System.err.println("Argument " + args[0] + " must be an integer. (i.e < 2147483647)");
-                System.exit(1);
+                System.err.println("Argument " + args[0] + " must be an integer.");
+                exit();
             }
             if (Arg1 < 0) {
                 System.err.println("Integer was less than 0, this cannot be converted");
-            }
-            else {
+                exit();
+            }else if(0 < Arg1 && Arg1 < 2147483647){
                 char[] ch = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
                 int rem, num;
                 num = Arg1;
@@ -38,21 +46,26 @@ public class Dec2Hex {
     public void test1(){
         String[] array = {};
         main(array);
+        assertTrue(Dec2Hex.exited);
     }
     @Test
     public void test2(){
         String[] array = {"24","27"};
         main(array);
+        assertTrue(Dec2Hex.exited);
     }
     @Test
     public void test3(){
         String[] array = {"Ryan's Test"};
         main(array);
+        assertTrue(Dec2Hex.exited);
     }
+
     @Test
     public void test4(){
         String[] array = {"-10"};
         main(array);
+        assertTrue(Dec2Hex.exited);
     }
     @Test
     public void test5(){
@@ -62,11 +75,11 @@ public class Dec2Hex {
     @Test
     public void test6(){
         String[] array = {"2147483647"};
-        main(array);
     }
     @Test
     public void test7(){
         String[] array = {"2147483648"};
         main(array);
+        assertTrue(Dec2Hex.exited);
     }
 }
