e604018b83f90fd19502a048dd114372e05c80c1
brianrown <rbrown232@caledonian.ac.uk>
2019-10-25 15:23:52 +0100

    Adjusting thanks to SonarQube highlighting smells

--- src/main/java/Dec2Hex.java
+++ src/main/java/Dec2Hex.java
@@ -1,6 +1,12 @@
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+
+import static java.util.logging.Level.INFO;
+
 public class Dec2Hex {
-    private static int Arg1;
+    private static int arg1;
     static boolean exited;
+    private static Logger logger = Logger.getLogger(String.valueOf(Dec2Hex.class));
 
     private static void exit() {
         exited = true;
@@ -13,21 +19,21 @@ public class Dec2Hex {
         }
         else {
             try {
-                Arg1 = Integer.parseInt(args[0]);
+                arg1 = Integer.parseInt(args[0]);
             } catch (NumberFormatException e) {
-                System.err.println("Argument " + args[0] + " must be an integer.");
+                logger.log(new LogRecord(INFO,"Argument " + args[0] + " must be an integer."));
                 exit();
             }
-            if (Arg1 < 0) {
-                System.err.println("Integer was less than 0, this cannot be converted");
+            if (arg1 < 0) {
+                logger.log(new LogRecord(INFO,"Integer was less than 0, this cannot be converted"));
                 exit();
-            }else if(0 < Arg1 && Arg1 < 2147483647){
+            }else if(0 < arg1 && arg1 < 2147483647){
                 char[] ch = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-                int rem, num;
-                num = Arg1;
+                int rem;
+                int num;
+                num = arg1;
                 StringBuilder hexadecimal = new StringBuilder();
-                System.out.println("Converting the Decimal Value " + num + " to Hex...");
-
+                logger.log(new LogRecord(INFO,"Converting the Decimal Value " + num + " to Hex..."));
                 while (num != 0) {
                     rem = num % 16;
                     hexadecimal.insert(0, ch[rem]);
